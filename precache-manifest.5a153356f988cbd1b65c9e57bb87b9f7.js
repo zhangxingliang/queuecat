99db0086edcbacb6bd28c815ba20e8f5e0fd6100
zhangxingliang <609649643@qq.com>
2019-04-13 21:40:26 +0800

    gg

---
+++ precache-manifest.5a153356f988cbd1b65c9e57bb87b9f7.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = [
+  {
+    "revision": "2807abcac65e4d87f260b1eb00d99f64",
+    "url": "/queuecat-pc/image/20193102249128551_14.jpg"
+  },
+  {
+    "revision": "06e13ba3319bacd5c23ec16898b734ac",
+    "url": "/queuecat-pc/json/news.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/queuecat-pc/robots.txt"
+  },
+  {
+    "revision": "8c6bb6071a5c326552cf5e156e89a606",
+    "url": "/queuecat-pc/image/20193102249128551_17.jpg"
+  },
+  {
+    "revision": "5aa4612bd9b96645a29b",
+    "url": "/queuecat-pc/js/app.9d671f2f.js"
+  },
+  {
+    "revision": "ad44a118a6f70f546daf",
+    "url": "/queuecat-pc/js/brand.92f9b0d4.js"
+  },
+  {
+    "revision": "22b5e55b3b3bdcc773b3",
+    "url": "/queuecat-pc/js/chunk-vendors.1d9a510d.js"
+  },
+  {
+    "revision": "45b354d4127e568b9f76043efdc3681f",
+    "url": "/queuecat-pc/json/contact.json"
+  },
+  {
+    "revision": "ab8fcb1799477948d315",
+    "url": "/queuecat-pc/js/contact.5306a28e.js"
+  },
+  {
+    "revision": "b706a46ca4511ed5cd55d08884485689",
+    "url": "/queuecat-pc/json/advantage.json"
+  },
+  {
+    "revision": "63e4ca39a0cf8a21a10a",
+    "url": "/queuecat-pc/js/join.10a3a1da.js"
+  },
+  {
+    "revision": "5acbbc24f4e8d4b2caccc54e1f44f441",
+    "url": "/queuecat-pc/json/join.json"
+  },
+  {
+    "revision": "4000e095c0771160af67",
+    "url": "/queuecat-pc/js/news.0c5fe1e1.js"
+  },
+  {
+    "revision": "987f80ad348c190177d0ee238b0b6085",
+    "url": "/queuecat-pc/json/brand.json"
+  },
+  {
+    "revision": "fc54e246d9e9d19fced1c67b4c048272",
+    "url": "/queuecat-pc/json/about.json"
+  },
+  {
+    "revision": "76d2926d4004e1e40b53f761b6f51498",
+    "url": "/queuecat-pc/image/20193102249128551_2.jpg"
+  },
+  {
+    "revision": "ec37fc6dc124163d036629fed71d614c",
+    "url": "/queuecat-pc/img/20193102249128551_16.ec37fc6d.jpg"
+  },
+  {
+    "revision": "76ce57de99a76a6ce229bb5305cd07c4",
+    "url": "/queuecat-pc/img/20193102249128551_6.76ce57de.jpg"
+  },
+  {
+    "revision": "372578bf098b02b5d2eab348404802a8",
+    "url": "/queuecat-pc/img/bg.372578bf.jpg"
+  },
+  {
+    "revision": "0a56db54b8d1c176f070d58d509d4c8e",
+    "url": "/queuecat-pc/img/20193102249128551_4.0a56db54.jpg"
+  },
+  {
+    "revision": "7498258772d85b0d3cff69d4e736af5c",
+    "url": "/queuecat-pc/img/20193102249128551_9.74982587.jpg"
+  },
+  {
+    "revision": "1a9495978018a1dd4ca7b6ab01ce831e",
+    "url": "/queuecat-pc/img/20193102249128551_21.1a949597.jpg"
+  },
+  {
+    "revision": "e52c1ee8e3dad4dd3a8267f9c727a96a",
+    "url": "/queuecat-pc/img/20193102249128551_18.e52c1ee8.jpg"
+  },
+  {
+    "revision": "92a1ed9864c8132c7c83",
+    "url": "/queuecat-pc/js/about.7552c34b.js"
+  },
+  {
+    "revision": "125726576a8e075d4b6d8ca2b68019e9",
+    "url": "/queuecat-pc/index.html"
+  },
+  {
+    "revision": "58ba7b71c93a07e1c632ea7c97a46109",
+    "url": "/queuecat-pc/img/20193102249128551_3.58ba7b71.jpg"
+  },
+  {
+    "revision": "de0b091f69fc3bc9603d4057661f76ae",
+    "url": "/queuecat-pc/img/20193102249128551_8.de0b091f.jpg"
+  },
+  {
+    "revision": "95c48c18641474cd254234c9f12d6190",
+    "url": "/queuecat-pc/img/qr.95c48c18.jpg"
+  },
+  {
+    "revision": "83871b06f8ed76d320870fb0473b8444",
+    "url": "/queuecat-pc/img/banner.83871b06.png"
+  },
+  {
+    "revision": "76d2926d4004e1e40b53f761b6f51498",
+    "url": "/queuecat-pc/img/20193102249128551_2.76d2926d.jpg"
+  },
+  {
+    "revision": "7455a2a47e32f852898772174671b62a",
+    "url": "/queuecat-pc/img/20193102249128551_22.7455a2a4.jpg"
+  },
+  {
+    "revision": "8b917a88e48b06f7db227d9e8d40d5e6",
+    "url": "/queuecat-pc/img/20193102249128551_20.8b917a88.jpg"
+  },
+  {
+    "revision": "c04ed18aa56a67a05dcdb4cf7325e441",
+    "url": "/queuecat-pc/img/20193102249128551_7.c04ed18a.jpg"
+  },
+  {
+    "revision": "1796d57af43212d97a47837d9c51336e",
+    "url": "/queuecat-pc/img/logo.1796d57a.jpg"
+  },
+  {
+    "revision": "8c6bb6071a5c326552cf5e156e89a606",
+    "url": "/queuecat-pc/img/20193102249128551_17.8c6bb607.jpg"
+  },
+  {
+    "revision": "4a49210e61c5ce72b0e24b765ac0d501",
+    "url": "/queuecat-pc/img/20193102249128551_23.4a49210e.jpg"
+  },
+  {
+    "revision": "6071931076ba5a392f6f91525a9b0a9b",
+    "url": "/queuecat-pc/img/default.60719310.jpg"
+  },
+  {
+    "revision": "128ad3fcf253ea554d35036a8fed14a1",
+    "url": "/queuecat-pc/img/20193102249128551_5.128ad3fc.jpg"
+  },
+  {
+    "revision": "05b825088beebf54d23a15735e05d5b5",
+    "url": "/queuecat-pc/img/20193102249128551_19.05b82508.jpg"
+  },
+  {
+    "revision": "7498258772d85b0d3cff69d4e736af5c",
+    "url": "/queuecat-pc/image/20193102249128551_9.jpg"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/queuecat-pc/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "4787201880c6dd606a0a747ad6d3dd4d",
+    "url": "/queuecat-pc/img/20193102249128551_11.47872018.jpg"
+  },
+  {
+    "revision": "aafa90c16a3507107012ed4f7256da3b",
+    "url": "/queuecat-pc/img/20193102249128551_13.aafa90c1.jpg"
+  },
+  {
+    "revision": "46862a5082bd2533e258efdf6523d8b0",
+    "url": "/queuecat-pc/img/20193102249128551_1.46862a50.jpg"
+  },
+  {
+    "revision": "46862a5082bd2533e258efdf6523d8b0",
+    "url": "/queuecat-pc/image/20193102249128551_1.jpg"
+  },
+  {
+    "revision": "eeef9bb0de8d31d68a7115cb86a34cb3",
+    "url": "/queuecat-pc/image/1.jpg"
+  },
+  {
+    "revision": "9f2aab7a199c458106ba704e06acf01f",
+    "url": "/queuecat-pc/image/2.jpg"
+  },
+  {
+    "revision": "4787201880c6dd606a0a747ad6d3dd4d",
+    "url": "/queuecat-pc/image/20193102249128551_11.jpg"
+  },
+  {
+    "revision": "54c762c401ab82b8bc3eb93d3d5ea8d3",
+    "url": "/queuecat-pc/image/20193102249128551_10.jpg"
+  },
+  {
+    "revision": "b21ef58730738de34202692c3e7b0b83",
+    "url": "/queuecat-pc/image/20193102249128551_12.jpg"
+  },
+  {
+    "revision": "aafa90c16a3507107012ed4f7256da3b",
+    "url": "/queuecat-pc/image/20193102249128551_13.jpg"
+  },
+  {
+    "revision": "2807abcac65e4d87f260b1eb00d99f64",
+    "url": "/queuecat-pc/img/20193102249128551_14.2807abca.jpg"
+  },
+  {
+    "revision": "ec37fc6dc124163d036629fed71d614c",
+    "url": "/queuecat-pc/image/20193102249128551_16.jpg"
+  },
+  {
+    "revision": "b21ef58730738de34202692c3e7b0b83",
+    "url": "/queuecat-pc/img/20193102249128551_12.b21ef587.jpg"
+  },
+  {
+    "revision": "a24330d04d0ea1d6acc72d59bf50b6f9",
+    "url": "/queuecat-pc/image/20193102249128551_15.jpg"
+  },
+  {
+    "revision": "05b825088beebf54d23a15735e05d5b5",
+    "url": "/queuecat-pc/image/20193102249128551_19.jpg"
+  },
+  {
+    "revision": "8b917a88e48b06f7db227d9e8d40d5e6",
+    "url": "/queuecat-pc/image/20193102249128551_20.jpg"
+  },
+  {
+    "revision": "e52c1ee8e3dad4dd3a8267f9c727a96a",
+    "url": "/queuecat-pc/image/20193102249128551_18.jpg"
+  },
+  {
+    "revision": "1a9495978018a1dd4ca7b6ab01ce831e",
+    "url": "/queuecat-pc/image/20193102249128551_21.jpg"
+  },
+  {
+    "revision": "54c762c401ab82b8bc3eb93d3d5ea8d3",
+    "url": "/queuecat-pc/img/20193102249128551_10.54c762c4.jpg"
+  },
+  {
+    "revision": "4a49210e61c5ce72b0e24b765ac0d501",
+    "url": "/queuecat-pc/image/20193102249128551_23.jpg"
+  },
+  {
+    "revision": "7455a2a47e32f852898772174671b62a",
+    "url": "/queuecat-pc/image/20193102249128551_22.jpg"
+  },
+  {
+    "revision": "0a56db54b8d1c176f070d58d509d4c8e",
+    "url": "/queuecat-pc/image/20193102249128551_4.jpg"
+  },
+  {
+    "revision": "128ad3fcf253ea554d35036a8fed14a1",
+    "url": "/queuecat-pc/image/20193102249128551_5.jpg"
+  },
+  {
+    "revision": "58ba7b71c93a07e1c632ea7c97a46109",
+    "url": "/queuecat-pc/image/20193102249128551_3.jpg"
+  },
+  {
+    "revision": "76ce57de99a76a6ce229bb5305cd07c4",
+    "url": "/queuecat-pc/image/20193102249128551_6.jpg"
+  },
+  {
+    "revision": "de0b091f69fc3bc9603d4057661f76ae",
+    "url": "/queuecat-pc/image/20193102249128551_8.jpg"
+  },
+  {
+    "revision": "c04ed18aa56a67a05dcdb4cf7325e441",
+    "url": "/queuecat-pc/image/20193102249128551_7.jpg"
+  },
+  {
+    "revision": "b8e180b4dcc41df4c276a8f8d0e1f7a4",
+    "url": "/queuecat-pc/image/3.jpg"
+  },
+  {
+    "revision": "3d3e24cdf4732932e8dfc8d5c32e5ba6",
+    "url": "/queuecat-pc/image/5.jpg"
+  },
+  {
+    "revision": "b651f4461178e0f56eb4de3676419636",
+    "url": "/queuecat-pc/image/4.jpg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/queuecat-pc/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "acbe2b994cc4523b3d90d6b4f27b5332",
+    "url": "/queuecat-pc/image/5.png"
+  },
+  {
+    "revision": "2a533841232a18c52eed2d42d00b033b",
+    "url": "/queuecat-pc/image/4.png"
+  },
+  {
+    "revision": "d369f9eee95b212542c1987e7a44f5e9",
+    "url": "/queuecat-pc/image/6.jpeg"
+  },
+  {
+    "revision": "acbe2b994cc4523b3d90d6b4f27b5332",
+    "url": "/queuecat-pc/image/7.png"
+  },
+  {
+    "revision": "6febab88acf9e33720fc0b794594996d",
+    "url": "/queuecat-pc/image/6.jpg"
+  },
+  {
+    "revision": "83871b06f8ed76d320870fb0473b8444",
+    "url": "/queuecat-pc/image/banner.png"
+  },
+  {
+    "revision": "6071931076ba5a392f6f91525a9b0a9b",
+    "url": "/queuecat-pc/image/default.jpg"
+  },
+  {
+    "revision": "372578bf098b02b5d2eab348404802a8",
+    "url": "/queuecat-pc/image/bg.jpg"
+  },
+  {
+    "revision": "6071931076ba5a392f6f91525a9b0a9b",
+    "url": "/queuecat-pc/image/f10.jpg"
+  },
+  {
+    "revision": "1e6cb58eae09eda7891fd59ba7728e3c",
+    "url": "/queuecat-pc/image/f11.jpg"
+  },
+  {
+    "revision": "ccee9bb77ab6f750cf3249deb98aa96b",
+    "url": "/queuecat-pc/image/f2.jpg"
+  },
+  {
+    "revision": "15fd3f9afc72998c290a25824bb1eca1",
+    "url": "/queuecat-pc/image/f1.jpg"
+  },
+  {
+    "revision": "c22331c73577ee56c51aaec8f0fbe3db",
+    "url": "/queuecat-pc/image/f4.jpg"
+  },
+  {
+    "revision": "836d01f1f78d81a8a014b2472a1fac6b",
+    "url": "/queuecat-pc/image/f3.jpg"
+  },
+  {
+    "revision": "9f9d83ab4c6095557077a1a428aa447c",
+    "url": "/queuecat-pc/image/f5.jpg"
+  },
+  {
+    "revision": "d7f215b3ad8d36db7e091c561c7a6a6d",
+    "url": "/queuecat-pc/image/f6.jpg"
+  },
+  {
+    "revision": "1984f3cb4c3ef59704b1e534e7c1b4d6",
+    "url": "/queuecat-pc/image/f7.jpg"
+  },
+  {
+    "revision": "0742ccc4f05b03853f19232185610899",
+    "url": "/queuecat-pc/image/footer-bg.png"
+  },
+  {
+    "revision": "a9d1ea47e31f5f6fef42d342d758b5bf",
+    "url": "/queuecat-pc/image/hislider-arrow.png"
+  },
+  {
+    "revision": "c7114c5a77fa609e70d937992b46024e",
+    "url": "/queuecat-pc/image/f8.jpg"
+  },
+  {
+    "revision": "f4ca580fdcc9d7ce23c16cf945dcefdf",
+    "url": "/queuecat-pc/image/f9.jpg"
+  },
+  {
+    "revision": "95c48c18641474cd254234c9f12d6190",
+    "url": "/queuecat-pc/image/qr.jpg"
+  },
+  {
+    "revision": "fc073ea72df90583562eadef89c7e1d5",
+    "url": "/queuecat-pc/image/phone-icon.png"
+  },
+  {
+    "revision": "1796d57af43212d97a47837d9c51336e",
+    "url": "/queuecat-pc/image/logo.jpg"
+  },
+  {
+    "revision": "a24330d04d0ea1d6acc72d59bf50b6f9",
+    "url": "/queuecat-pc/img/20193102249128551_15.a24330d0.jpg"
+  },
+  {
+    "revision": "4000e095c0771160af67",
+    "url": "/queuecat-pc/css/news.092f55b9.css"
+  },
+  {
+    "revision": "63e4ca39a0cf8a21a10a",
+    "url": "/queuecat-pc/css/join.ea9e4a00.css"
+  },
+  {
+    "revision": "ab8fcb1799477948d315",
+    "url": "/queuecat-pc/css/contact.f0a9c805.css"
+  },
+  {
+    "revision": "ad44a118a6f70f546daf",
+    "url": "/queuecat-pc/css/brand.fb662a47.css"
+  },
+  {
+    "revision": "5aa4612bd9b96645a29b",
+    "url": "/queuecat-pc/css/app.926bf0be.css"
+  },
+  {
+    "revision": "92a1ed9864c8132c7c83",
+    "url": "/queuecat-pc/css/about.8f237431.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/queuecat-pc/precache-manifest.5a153356f988cbd1b65c9e57bb87b9f7.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "queuecat-pc"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
